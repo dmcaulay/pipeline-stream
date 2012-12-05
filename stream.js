660af35f1c8a278afd5e026abb8a6dee6800fa23
Dan McAulay <dmcaulay@gmail.com>
2012-12-04 17:57:51 -0800

    change the comments on the stream class

--- stream.js
+++ stream.js
@@ -5,30 +5,56 @@
 // i've decided to remove them because i think they complicate the node.js
 // stream class
 //
-// control flow is currently implemented with the drain event.
-// any stream that implements control flow waits for the drain event
-// before sending data down the pipe. all other streams ignore the drain
-// event and emit it on a next, error or noop event
+// basic stream layout
+// s - source
+// q - queue
+// m - mddleware
 //
-// we have a few different stream types to think about
-// 1) all streams
-//   - emit 'error' when there is an error
-//     - all streams assume that an error means you had an issue handling data
-//     - this means you dropped the data and you're ready for more
-//     - you can see that the on error method emits 'drain'
-// 2) control flow stream (usually a queue)
-//   - in this case you're always willing to accept data so you always
-//     emit drain in the onData method
-//   - you implement onDrain. this gets called when you can send
-//     more data down the pipe
-//   - you call emit('data') when you send data down the pipe
-// 3) normal stream
-//   - you emit('next', data) when you're done and ready to send data down the pipe
-//   - you emit('noop') if you need more data before sending anything down the pipe
-//     - filter or batch job
-//   - you allow max number of items to queue up by calling source.onDrain in the
-//     onData method while count < max
-//   - on the drain event you decrement count and call source.onDrain if count < max
+// s->q->m->q->m->...
+//
+// streams emit data when sending down the pipe and emit drain when they are 
+// ready for more data. lets go over how this works for all 3 of the types.
+//
+// you'll see that the queue is it's own stream. i prefer this because it
+// keeps the logic out of the middleware streams and makes it easy to move
+// the queue out of memory to something like sqs or redis
+//
+// source:
+// the source stream reads data as fast as it wants and sends it down the pipe.
+// this stream doesn't need to emit drain because there is noone upstram.
+//
+// queue:
+// queues receive data, but only emit data after their onDrain method is called. 
+// there is one exception to this rule and that's at startup. we assume that
+// the downstream stream can handle the first chunk of data before it calls drain.
+//
+// middleware:
+// this is where everything interesting happens. most appication developers will
+// only write middleware streams. they receive data do their job and emit new
+// data to the downstream stream. they are also responsible for calling drain when they're
+// done with the data.
+//
+// events:
+// 'data' emitted to send data down the pipe
+// 'drain' emitted to indicate the stream is done processing data
+// 'error' emitted to indicate something went wrong while processing this chunk of
+// data. as a convenience, when you pipe one stream to another stream we add a
+// listener on the error event that automatically calls drain so the upstream
+// queue knows to send more data.
+// 'next' emitted to send data and emit the drain event
+// 'noop' emitted to indicate the streram is ignoring this data and not sending
+// it down the pipe
+//
+// options
+// name: the name of the stream
+// max: the max number of items to write in parallel
+// reporter: an object used to report data, error,
+// noop events
+//
+// methods
+// pipe: same as node.js
+// getStats: get an containing the current stats
+// resetStats: reset the stats
 //
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
@@ -84,10 +110,7 @@ Stream.prototype.pipe = function(dest) {
 
     if (dest.reporter) dest.reporter.on('data', flow, data);
 
-    if (dest.onDrain) {
-      // always drain if control flow stream
-      dest.emit('drain');
-    } else if (source.onDrain) {
+    if (source.onDrain) {
       // allow the source to queue up max number of
       // items to flow down the pipe
       if (dest.count < dest.max) source.onDrain();
