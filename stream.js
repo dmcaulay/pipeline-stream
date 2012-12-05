15feff3bbee8c7401c17d8b9eaf57b7b36fb8606
Dan McAulay <dmcaulay@gmail.com>
2012-12-05 14:39:33 -0800

    remove comments on the base stream class

--- stream.js
+++ stream.js
@@ -1,61 +1,6 @@
 //
 // inspired by node.js stream
 //
-// there is no concept of pause, resume, end or close 
-// i've decided to remove them because i think they complicate the node.js
-// stream class
-//
-// basic stream layout
-// s - source
-// q - queue
-// m - mddleware
-//
-// s->q->m->q->m->...
-//
-// streams emit data when sending down the pipe and emit drain when they are 
-// ready for more data. lets go over how this works for all 3 of the types.
-//
-// you'll see that the queue is it's own stream. i prefer this because it
-// keeps the logic out of the middleware streams and makes it easy to move
-// the queue out of memory to something like sqs or redis
-//
-// source:
-// the source stream reads data as fast as it wants and sends it down the pipe.
-// this stream doesn't need to emit drain because there is noone upstram.
-//
-// queue:
-// queues receive data, but only emit data after their onDrain method is called. 
-// there is one exception to this rule and that's at startup. we assume that
-// the downstream stream can handle the first chunk of data before it calls drain.
-//
-// middleware:
-// this is where everything interesting happens. most appication developers will
-// only write middleware streams. they receive data do their job and emit new
-// data to the downstream stream. they are also responsible for calling drain when they're
-// done with the data.
-//
-// events:
-// 'data' emitted to send data down the pipe
-// 'drain' emitted to indicate the stream is done processing data
-// 'error' emitted to indicate something went wrong while processing this chunk of
-// data. as a convenience, when you pipe one stream to another stream we add a
-// listener on the error event that automatically calls drain so the upstream
-// queue knows to send more data.
-// 'next' emitted to send data and emit the drain event
-// 'noop' emitted to indicate the streram is ignoring this data and not sending
-// it down the pipe
-//
-// options
-// name: the name of the stream
-// max: the max number of items to write in parallel
-// reporter: an object used to report data, error,
-// noop events
-//
-// methods
-// pipe: same as node.js
-// getStats: get an containing the current stats
-// resetStats: reset the stats
-//
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 
